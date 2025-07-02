3765a77b49816621f15756397e500d330e373e7a
Wicky2001 <pmwickramaratna@gmail.com>
2025-07-02 15:41:16 +0530

    successfully connected the wallet

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,45 @@
 import express, { Request, Response } from "express";
-
+import * as dotenv from "dotenv";
+import { BlockfrostProvider, MeshWallet } from "@meshsdk/core";
+dotenv.config();
 const app = express();
 const port = 3000;
 
-// Define a route handler for the default home page
+const block_forest_key = process.env.BLOCK_FOREST_API_KEY as string;
+const mnemonic = process.env.LACE_MEMONIC?.split(" ") as Array<string>;
+
+if (!block_forest_key || block_forest_key.trim() === "") {
+  throw new Error("BLOCK FOREST API KEY IS NOT DEFINED OR EMPTY");
+}
+if (!mnemonic) {
+  throw new Error("MNEMONIC KEYS ARE NOT DEFINED");
+}
+console.log(mnemonic);
+const provider = new BlockfrostProvider(block_forest_key);
+
+const wallet = new MeshWallet({
+  networkId: 0,
+  fetcher: provider,
+  submitter: provider,
+  key: {
+    type: "mnemonic",
+    words: mnemonic,
+  },
+});
+
+wallet
+  .getChangeAddress()
+  .then((address) => {
+    console.log(address);
+  })
+  .catch((error) => {
+    console.log("ERROR OCCUERED WHILE RETREIVING THE WALLET ADRESS {$error}");
+  });
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello, World! Hi wicky" });
 });
 
-// Start the Express server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
